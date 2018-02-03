47aca7a1a0269d544072d5b1c7210a2c961d6e14
Jerrick91 <36110154+Jerrick91@users.noreply.github.com>
2018-02-03 12:14:31 -0500

    Add files via upload

---
+++ sketch_feb03b.ino
@@ -0,0 +1,16 @@
+/*
+ * 
+ * Jordan Herrick 2/3/2018
+ * Turn led on and then off on Bluefruit Feather MO
+ */
+void setup() {
+ pinMode(13, OUTPUT);        //Sets up pin 13 to output signal
+}
+
+
+void loop() {
+digitalWrite(13, HIGH);      //Send signal to pin 13 HIGH is on
+delay(2000);                 //Wait 2 Seconds
+digitalWrite(13, LOW);       //Cuts signal to pin 13 Low is off
+delay(2000);}                //Wait 2 seconds
+
